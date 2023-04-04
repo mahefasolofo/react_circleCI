9a7d3250f7b1d479b4c34cbb988b30e59d5e93e0
mahefasolofo <mahefa.rasolofonirina@gmail.com>
2023-04-04 10:36:05 +0300

    tests diminués

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+    video: false,
+    pageLoadTimeout: 12000,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
